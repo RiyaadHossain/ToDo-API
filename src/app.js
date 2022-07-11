8deec86d8147ee02ac0876dac4dd749c2685e384
Riyaad_Hossain <riyadhossain.dev@gmail.com>
2022-07-11 11:32:02 +0600

    Post method added

--- src/app.js
+++ src/app.js
@@ -1,13 +1,18 @@
 // Set Up Server
 const express = require('express');
 const app = express()
+const todoRouter = require("./routes/route")
 
-require("./db/connect")
+require("./db/connect") // Connect to MongoDb
 
 app.get("/", (req, res) =>{
     res.send("Hello World")
 })
 
-app.listen(6000, () => {
+// Middlewares
+app.use(express.json())
+app.use("/todo", todoRouter)
+
+app.listen(5000, () => {
     console.log(`Server is running PORT 6000`);
 })
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,8 +1,34 @@
-const express = require('express');
-const ToDo = require("../models/task")
-const router = express.Router()
+const express = require("express");
+const ToDo = require("../models/task");
+const router = express.Router();
 
 // Get All the ToDo
-router.get("/", (req, res) => {
+router.get("/", (req, res) => {});
 
-})
+// Post a Task
+router.post("/", async (req, res) => {
+  try {
+    const todo = new ToDo(req.body);
+    await todo.save();
+    res.status(200).json({ message: " Task assigned successfully" });
+  } catch (error) {
+    res.status(400).json({
+      error: "A Server Error Occured",
+    });
+  }
+});
+
+// Post Multiple Tasks
+router.post("/tasks", (req, res) => {
+  ToDo.insertMany(req.body, (error, data) => {
+    if (error) {
+      res.status(400).json({
+        error,
+      });
+    } else {
+      res.status(200).json({ message: "ToDos are assigned successfully" });
+    }
+  });
+});
+
+module.exports = router;
