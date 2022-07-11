a238eee33313d147f9229db09e36996d936dd3cf
Riyaad_Hossain <riyadhossain.dev@gmail.com>
2022-07-11 20:06:11 +0600

    Some extra code removed

--- middlewares/verifyUser.js
+++ middlewares/verifyUser.js
@@ -5,9 +5,9 @@ const verifyUser = async (req, res, next) =>{
     try {
         const token = req.headers.authorization.split(" ")[1]
         const decoded = await jwt.verify(token, process.env.JWT_SECRET)
-        const user = await User.find({username: decoded})
-        req.name = user[0].name
-        req.email = user[0].email
+        const user = await User.findOne({username: decoded})
+        req.name = user.name
+        req.email = user.email
         next()
     } catch (error) {
         next("Opps! Authentication Failed.")
--- src/routes/userHandler.js
+++ src/routes/userHandler.js
@@ -26,11 +26,13 @@ router.post("/signup", async (req, res) => {
 // Log In
 router.post("/singin", async (req, res) => {
   try {
-    const user = await User.find({ username: req.body.username });
-    const isVerified = await bcrypt.compare(req.body.password, user[0].password);
-    
+    const user = await User.findOne({ username: req.body.username });
+    const isVerified = await bcrypt.compare(req.body.password, user.password);
+
     if (isVerified) {
-      const token = await jwt.sign(user[0].username, process.env.JWT_SECRET);
+      const token = await jwt.sign(user.username, process.env.JWT_SECRET, {
+        expiresIn: "1h",
+      });
       res.status(200).json({ token, message: "User Signed in Successfully" });
     } else {
       res.status(401).json({ error: "Authentication Error!" });
