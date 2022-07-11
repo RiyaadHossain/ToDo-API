f84dc95fc579de7c54c61247e84bfceaf07a27a8
Riyaad_Hossain <riyadhossain.dev@gmail.com>
2022-07-11 11:45:40 +0600

    GET api created

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,8 +2,34 @@ const express = require("express");
 const ToDo = require("../models/task");
 const router = express.Router();
 
-// Get All the ToDo
-router.get("/", (req, res) => {});
+// Get All the ToDos
+router.get("/", (req, res) => {
+  ToDo.find((err, data) => {
+    if (err) {
+      res.status(400).json({
+        error: "A Server Error Occured",
+      });
+    } else {
+      res.status(200).json({
+        data,
+      });
+    }
+  });
+});
+
+// Get a ToDo
+router.get("/:id", async (req, res) => {
+  try {
+    const data = await ToDo.findById(req.params.id);
+    res.status(200).json({
+      data,
+    });
+  } catch (error) {
+    res.status(400).json({
+      error: "A Server Error Occured",
+    });
+  }
+});
 
 // Post a Task
 router.post("/", async (req, res) => {
@@ -20,10 +46,10 @@ router.post("/", async (req, res) => {
 
 // Post Multiple Tasks
 router.post("/tasks", (req, res) => {
-  ToDo.insertMany(req.body, (error, data) => {
+  ToDo.insertMany(req.body, (error) => {
     if (error) {
       res.status(400).json({
-        error,
+        error: "A Server Error Occured",
       });
     } else {
       res.status(200).json({ message: "ToDos are assigned successfully" });
