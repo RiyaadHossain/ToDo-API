db8466a8732c9044d3bab499ac81eee1e887a022
Riyaad_Hossain <riyadhossain.dev@gmail.com>
2022-07-11 18:25:25 +0600

    Sign up Method added

--- src/routes/userHandler.js
+++ src/routes/userHandler.js
@@ -1,10 +1,31 @@
 const express = require("express");
 const User = require("../models/userModel");
 const router = express.Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 // Sign Up
-router.post("/signup", (req, res) => {
-    
+router.post("/signup", async (req, res) => {
+  try {
+    const password = await bcrypt.hash(req.body.password, 10);
+    const newUser = new User({
+      name: req.body.name,
+      username: req.body.username,
+      email: req.body.email,
+      password: password,
+    });
+    const user = await newUser.save();
+    if (user) {
+      res.status(200).json({ message: "User Signed up successfully." });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "A server Error Occured." });
+  }
 });
+
+// Log In
+router.post("/singin", async( req, res) => {
+
+})
 
 module.exports = router;
